b0c0afe6aa5c4714a320320c8a99d439c0b5dc55
DarrenCHoding <darrencornelius2005@gmail.com>
2025-05-11 00:15:50 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -7,13 +7,13 @@ const variables = {
     night: "Good Night",
   },
   quotes: [
-    { quote: "I'm sorry for loving you.", author: "standinhsd" },
-    { quote: "I love you.", author: "Brian" },
-    { quote: "Aku sayang kamu!", author: "Ian" },
-    { quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
-    { quote: "Love chuuu", author: "Bribri" },
+    { quote: "I'm sorry for loving you.", author: "Darren" },
+    { quote: "I love you.", author: "Derr" },
+    { quote: "Aku sayang kamu!", author: "Abang" },
+    { quote: "Ian sayang Ava ♥", author: "Sayangnya kamu" },
+    { quote: "Love chuuu", author: "Cinta" },
   ],
-  nicknames: ["Ava", "Avakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
+  nicknames: ["Nina", "Nina Cantik", "Nina Sayang", "Dudud", "Nitnut", "Cintaku Sayangku Cantikku NAF"],
   generated: ["{greetings}, {nicknames}!"],
 };
 
